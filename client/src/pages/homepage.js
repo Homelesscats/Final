5b516a8e2f6a1188c0326bba097a9f00ec73dba1
apatiga <128877342+apatiga@users.noreply.github.com>
2023-09-14 20:57:51 -0700

    made changes to the homepage -robert

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,23 +1,35 @@
-import { useContext } from "react";
-
+import React, { useContext } from "react";
 import { AuthContext } from "../context/authContext";
-
+import { Link } from "react-router-dom";
 function Homepage() {
   const { user } = useContext(AuthContext);
   return (
-    <>
-      <h1>This is the Homepage</h1>
-      {user ? (
-        <>
-          <h2>{user.email} is now logged in </h2>
-        </>
-      ) : (
-        <>
-          <p>There is no user data</p>
-        </>
-      )}
-    </>
+    <div className="homepage-container">
+      <div className="content">
+        <h1>Welcome to the Homepage</h1>
+        {user ? (
+          // If a user is logged in, display user information
+          <>
+            <h2>{user.email} is now logged in</h2>
+            <ul>
+              <li>
+                <Link to="/link1">Link 1</Link>
+              </li>
+              <li>
+                <Link to="/link2">Link 2</Link>
+              </li>
+              <li>
+                <Link to="/link3">Link 3</Link>
+              </li>
+            </ul>
+          </>
+        ) : null}
+        <input
+          type="text"
+          placeholder="Please enter your information here"
+        />
+      </div>
+    </div>
   );
 }
-
 export default Homepage;
