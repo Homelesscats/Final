0bfd395ee3e75eef029c88acaf93a37fcc7dfbc4
Lrosa0126 <Andypanther1@gmail.com>
2023-09-18 00:17:10 -0700

    Fixed a random break in the page by adding a USER check?

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -14,39 +14,45 @@ function Homepage() {
     setComment(event.target.value);
   };
 
-const handleSubmitComment = () => {
-  if (user && user.email) {
-    const newComment = {
-      text: comment,
-      id: comments.length + 1,
-      timestamp: new Date().toLocaleString(),
-      email: user.email, // Use user's email here
-    };
+  const handleSubmitComment = () => {
+    if (user && user.email) {
+      const newComment = {
+        text: comment,
+        id: comments.length + 1,
+        timestamp: new Date().toLocaleString(),
+        email: user.email, // Use user's email here
+      };
 
-    // Add the new comment to the state
-    setComments([...comments, newComment]);
+      // Add the new comment to the state
+      setComments([...comments, newComment]);
 
-    // Clear the comment input
-    setComment("");
-  }
-};
+      // Clear the comment input
+      setComment("");
+    }
+  };
 
+  const imageSources = [
+    "beagle.jpg",
+    "happy.jpg",
+    "pom.jpg",
+    // Add more image URLs as needed
+  ];
   const links = [
     {
       id: 1,
-      img: "pages/beagle.jpg", // Update the image source for Link 1
+      img: imageSources[0], // Update the image source for Link 1
       description: "Description for Link 1",
       to: "https://buy.stripe.com/test_5kA5oq5JEbnw8485kl",
     },
     {
       id: 2,
-      imgc: "happ1.jpg", // Update the image source for Link 2
+      img: imageSources[1], // Update the image source for Link 2
       description: "Description for Link 2",
       to: "https://buy.stripe.com/test_8wMdUWega3V41FKcMO",
     },
     {
       id: 3,
-      img: "pom.jpg", // Update the image source for Link 3
+      img: imageSources[2], // Update the image source for Link 3
       description: "Description for Link 3",
       to: "https://buy.stripe.com/test_4gweZ01to3V4cko8wz",
     },
@@ -55,11 +61,20 @@ const handleSubmitComment = () => {
   return (
     <div className="homepage-container">
       <div className="content">
-        <h1>Welcome to the Animal Liberation League {user.email}!</h1>
+        <h1>
+          Welcome to the Animal Liberation League{" "}
+          {user ? user.email : "Guest"}!
+        </h1>
         <h3>
-    Our mission at the Animal Liberation League is to empower compassionate individuals to make a positive impact on the lives of animals in need. We provide a central hub connecting supporters with organizations like the Beagle Freedom Project, OC Pom Rescue, and Friends of Orange County's Homeless Pets. Through awareness, support, and collaboration, we strive to bring freedom, safety, and happiness to animals, one rescue, one adoption, and one act of kindness at a time.
-  </h3>
-
+          Our mission at the Animal Liberation League is to empower
+          compassionate individuals to make a positive impact on the lives of
+          animals in need. We provide a central hub connecting supporters with
+          organizations like the Beagle Freedom Project, OC Pom Rescue, and
+          Friends of Orange County's Homeless Pets. Through awareness, support,
+          and collaboration, we strive to bring freedom, safety, and happiness
+          to animals, one rescue, one adoption, and one act of kindness at a
+          time.
+        </h3>
       </div>
 
       <div className="background-layout">
@@ -96,7 +111,10 @@ const handleSubmitComment = () => {
             {comments.slice().reverse().map((comment) => (
               <div key={comment.id} className="comment">
                 <p>
-                  <strong>{comment.email} {comment.timestamp}</strong>: {comment.text}
+                  <strong>
+                    {comment.email} {comment.timestamp}
+                  </strong>
+                  : {comment.text}
                 </p>
               </div>
             ))}
