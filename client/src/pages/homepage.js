c192174fc8a6321957e08eb5d29c298ca0fc7127
Lrosa0126 <Andypanther1@gmail.com>
2023-09-17 22:46:04 -0700

    Fixed comment section. Added time/email. and fixed scroll issue

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../context/authContext";
 import { Link } from "react-router-dom";
+
 import "./homepage.css";
 
 function Homepage() {
@@ -13,10 +14,13 @@ function Homepage() {
     setComment(event.target.value);
   };
 
-  const handleSubmitComment = () => {
+const handleSubmitComment = () => {
+  if (user && user.email) {
     const newComment = {
       text: comment,
-      id: comments.length + 1, // Generate a unique ID (you can use a library like uuid for this)
+      id: comments.length + 1,
+      timestamp: new Date().toLocaleString(),
+      email: user.email, // Use user's email here
     };
 
     // Add the new comment to the state
@@ -24,24 +28,25 @@ function Homepage() {
 
     // Clear the comment input
     setComment("");
-  };
+  }
+};
 
   const links = [
     {
       id: 1,
-      imageSrc: "/link1-image.jpg",
+      imageSrc: "pages/beagle.jpg", // Update the image source for Link 1
       description: "Description for Link 1",
       to: "https://buy.stripe.com/test_5kA5oq5JEbnw8485kl",
     },
     {
       id: 2,
-      imageSrc: "/link2-image.jpg",
+      imageSrc: "happ1.jpg", // Update the image source for Link 2
       description: "Description for Link 2",
       to: "https://buy.stripe.com/test_8wMdUWega3V41FKcMO",
     },
     {
       id: 3,
-      imageSrc: "/link3-image.jpg",
+      imageSrc: "pom.jpg", // Update the image source for Link 3
       description: "Description for Link 3",
       to: "https://buy.stripe.com/test_4gweZ01to3V4cko8wz",
     },
@@ -71,28 +76,33 @@ function Homepage() {
             </div>
           ))}
         </div>
-      </div>
 
-      <div className="comment-box">
-        <textarea
-          rows="4"
-          placeholder="Leave a comment..."
-          value={comment}
-          onChange={handleCommentChange}
-          className="comment-input"
-        ></textarea>
-        <button onClick={handleSubmitComment} className="comment-button">
-          Submit
-        </button>
-      </div>
-
-      {/* Display submitted comments */}
-      <div className="comments-section">
-        {comments.map((comment) => (
-          <div key={comment.id} className="comment">
-            {comment.text}
+        <div className="comments">
+          <div className="comment-container">
+            <div className="comment-box">
+              <textarea
+                rows="4"
+                placeholder="Leave a comment..."
+                value={comment}
+                onChange={handleCommentChange}
+                className="comment-input"
+              ></textarea>
+              <button onClick={handleSubmitComment} className="comment-button">
+                Submit
+              </button>
+            </div>
           </div>
-        ))}
+
+          <div className="comments-section">
+            {comments.slice().reverse().map((comment) => (
+              <div key={comment.id} className="comment">
+                <p>
+                  <strong>{comment.email} {comment.timestamp}</strong>: {comment.text}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
